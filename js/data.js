fbb7144d38b15383eab9d800aa847c6a0c73a2d6
Dang Pham <dangpham@mit.edu>
2014-11-17 16:20:06 -0500

    add axis and better formatting

--- js/data.js
+++ js/data.js
@@ -2,122 +2,122 @@
     var rapedData = [
         {
             "Situation": "raped",
-            "Population": "femaleu",
+            "Population": "Female Undergrad",
             "Percentage": 5,
             "Number": 38
         },
         {
             "Situation": "raped",
-            "Population": "maleu",
+            "Population": "Male Undergrad",
             "Percentage": 1,
             "Number": 5
         },
         {
             "Situation": "raped",
-            "Population": "femalegrad",
+            "Population": "Female Grad",
             "Percentage": 1,
             "Number": 5
         },
         {
             "Situation": "raped",
-            "Population": "malegrad",
+            "Population": "Male Grad",
             "Percentage": 0,
             "Number": 2
         }
     ];
     var stalkedData = [{
         "Situation": "stalked",
-        "Population": "femaleu",
+        "Population": "Female Undergrad",
         "Percentage": 14,
         "Number": 109
     },
     {
         "Situation": "stalked",
-        "Population": "maleu",
+        "Population": "Male Undergrad",
         "Percentage": 2,
         "Number": 16
     },
     {
         "Situation": "stalked",
-        "Population": "femalegrad",
+        "Population": "Female Grad",
         "Percentage": 9,
         "Number": 58
     },
     {
         "Situation": "stalked",
-        "Population": "malegrad",
+        "Population": "Male Grad",
         "Percentage": 3,
         "Number": 27
     }];
     var abusiveData = [{
         "Situation": "abusiveRelationship",
-        "Population": "femaleu",
+        "Population": "Female Undergrad",
         "Percentage": 8,
         "Number": 67
     },
     {
         "Situation": "abusiveRelationship",
-        "Population": "maleu",
+        "Population": "Male Undergrad",
         "Percentage": 4,
         "Number": 28
     },
     {
         "Situation": "abusiveRelationship",
-        "Population": "femalegrad",
+        "Population": "Female Grad",
         "Percentage": 4,
         "Number": 26
     },
     {
         "Situation": "abusiveRelationship",
-        "Population": "malegrad",
+        "Population": "Male Grad",
         "Percentage": 2,
         "Number": 24
     }];
     var harassedData = [{
         "Situation": "harassed",
-        "Population": "femaleu",
+        "Population": "Female Undergrad",
         "Percentage": 15,
         "Number": 122
     },
     {
         "Situation": "harassed",
-        "Population": "maleu",
+        "Population": "Male Undergrad",
         "Percentage": 4,
         "Number": 25
     },
     {
         "Situation": "harassed",
-        "Population": "femalegrad",
+        "Population": "Female Grad",
         "Percentage": 7,
         "Number": 47
     },
     {
         "Situation": "harassed",
-        "Population": "malegrad",
+        "Population": "Male Grad",
         "Percentage": 2,
         "Number": 22
     }];
     var assaultedData =[{
         "Situation": "assaulted",
-        "Population": "femaleu",
+        "Population": "Female Undergrad",
         "Percentage": 10,
         "Number": 81
     },
     {
         "Situation": "assaulted",
-        "Population": "maleu",
+        "Population": "Male Undergrad",
         "Percentage": 2,
         "Number": 13
     },
     {
         "Situation": "assaulted",
-        "Population": "femalegrad",
+        "Population": "Female Grad",
         "Percentage": 3,
         "Number": 19
     },
     {
         "Situation": "assaulted",
-        "Population": "malegrad",
+        "Population": "Male Grad",
         "Percentage": 1,
         "Number": 6
     }];
@@ -141,7 +141,8 @@
     function drawData(dataset){
 
         var BarScale = d3.scale.ordinal()
-                         .domain(d3.range(dataset.length))
+                         .domain(["Female Undergrad","Male Undergrad",
+                                  "Female Grad", "Male Grad"])
                          .rangeRoundBands([0,w],0.05);
         var yScale = d3.scale.linear()
                         .domain([0,d3.max(dataset,function(d){
@@ -151,12 +152,20 @@
         var axisScale = d3.scale.linear()
                         .domain([0,d3.max(dataset,function(d){
                             return d.Percentage;
-                        })+1])
+                        })])
                         .range([h,0]);
+
+        var xAxis = d3.svg.axis()
+                          .scale(BarScale)
+                          .orient("bottom");
+
         var yAxis = d3.svg.axis()
                           .scale(axisScale)
                           .orient("left")
-                          .ticks(6);
+                          .ticks(6)
+                          .tickFormat( function(d){
+                              return d +"%";
+                          });
         console.log(dataset);
         console.log(d3.max(dataset,function(d){
             return d.Percentage;
@@ -175,6 +184,9 @@
                 },
                 height: function(d) {
                     return yScale(d.Percentage);
+                },
+                fill: function(d){
+                    return "rgb("+(100 + d.Percentage *10)+",50, 50)";
                 }
             });
             svg.select(".axis")
@@ -186,8 +198,8 @@
         else {
             var svg = d3.select(".vis")
                     .append("svg")
-                    .attr("width", w+10)
-                    .attr("height",h+10);
+                    .attr("width", w+50)
+                    .attr("height",h+25);
 
             svg.selectAll("rect")
                .data(dataset)
@@ -197,7 +209,7 @@
                .duration(1500)
                .attr({
                    x: function(d,i){
-                       return 20+ BarScale(i);
+                       return 35+ BarScale(d.Population);
                    },
                    y: function(d) {
                        return h - yScale(d.Percentage) +5;
@@ -207,12 +219,16 @@
                        return yScale(d.Percentage);
                    },
                    fill: function(d){
-                       return "rgb("+(100 + d.Percentage *10)+",234, 144)";
+                       return "rgb("+(100 + d.Percentage *10)+",50, 50)";
                    }
                });
              svg.append("g")
                 .attr("class","axis")
-                .attr("transform", "translate(20,5)")
+                .attr("transform", "translate(35,5)")
                 .call(yAxis);
+            svg.append("g")
+                .attr("class","x axis")
+                .attr("transform", "translate(35,505)")
+                .call(xAxis);
            }
     }
